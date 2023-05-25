180f199bccc1fd5ef18ea58fdd340af5d7186417
HanhDinh1 <ms.myhanh89@gmail.com>
2023-05-24 22:58:54 -0400

    Create the outline for all bank accounts and interest rate

---
+++ src/bankaccountapp/Account.java
@@ -0,0 +1,9 @@
+package bankaccountapp;
+
+public abstract class Account implements IBaseRate {//We don't want to create Objects from Account Class=> makes it abstract
+	//List common properties for savings and checking accounts
+	
+	//Constructor to set base properties and initialize the account
+	
+	//List common methods
+}
---
+++ src/bankaccountapp/Savings.java
@@ -0,0 +1,9 @@
+package bankaccountapp;
+
+public class Savings extends Account {
+	//List properties specific to a Savings account
+	
+	//Constructor to initialize savings account properties
+		
+	//List any methods specific to savings account
+}
